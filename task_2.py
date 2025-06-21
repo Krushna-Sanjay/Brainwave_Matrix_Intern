b656fcf2916d4da21ce4737c360117307ef9b382
Krushna-Sanjay <krushnagheewala.edu16@gmail.com>
2025-06-21 21:32:58 +0530

    Updated

---
+++ task_2.py
@@ -0,0 +1,21 @@
+# import nltk
+# nltk.download('vader_lexicon')
+# from nltk.sentiment import SentimentIntensityAnalyzer
+# import pandas as pd
+# import snscrape.modules.twitter as sntwitter
+
+# # sia = SentimentIntensityAnalyzer()
+# # sia.polarity_score()['compound']
+
+# # query = "Air India crash OR AI171 OR Ahmedabad lang:en since:2025-06-12 until:2025-06-20"
+# # tweets = []
+
+# # for i,tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+#     # if i>1000:
+#     #     break
+#     # tweets.append([tweet.date, tweet.content])
+
+# # df = pd.DataFrame(tweets, columns=['Date', 'Tweets'])
+# # df.to_csv('raw_tweets.csv', index=False)
+# # print(df.head())
+
